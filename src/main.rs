da29f6796ce2b61bcbacf51b59b6689a3d3610e5
Christoph J. Scherr <Christoph.Scherr@newtec.de>
2024-07-09 11:58:12 +0200

    feat: minimal clock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
+};
+use std::io;
+
+const TITLE: &str = "Crock";
+
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    loop {
+        terminal.draw(|f| {
+            let size = f.size();
+            let block = Block::default().title(TITLE).borders(Borders::ALL);
+            let inner = block.inner(size);
+            f.render_widget(block, size);
+            let w = ratatui::widgets::Paragraph::new(format!("{}", chrono::Utc::now()));
+            f.render_widget(w, inner);
+        })?;
+        if let Event::Key(key) = event::read()? {
+            if key.code == KeyCode::Char('q')
+                || key.code == KeyCode::Esc
+                || (key.modifiers.contains(KeyModifiers::CONTROL) && key.code == KeyCode::Char('c'))
+            {
+                break;
+            }
+        }
+    }
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
