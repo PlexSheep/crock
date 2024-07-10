e186a717f245be4570510dd8c47e49ea3ddce4e9
cscherrNT <cscherrNT@users.noreply.github.com>
2024-07-10 10:40:51 +0000

    automatic cargo CI changes

--- src/clock.rs
+++ src/clock.rs
@@ -66,7 +66,7 @@ pub struct Clock {
 }
 
 #[derive(Debug, Clone, PartialEq, Default)]
-pub(crate) struct UiData {
+pub struct UiData {
     fdate: [String; 2],
     ftime: [String; 2],
     timebar_ratio: [Option<f64>; 2],
